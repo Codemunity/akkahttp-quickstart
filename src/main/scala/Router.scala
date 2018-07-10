009ab4ef3f91b7fd7a41dd612fe317d368d7276c
Miguel Lopez <miguell@cakesolutions.net>
2018-07-10 14:34:50 +0100

    Initial router

---
+++ src/main/scala/Router.scala
@@ -0,0 +1,18 @@
+import akka.http.scaladsl.server.{Directives, Route}
+
+trait Router {
+  def route: Route
+}
+
+class TodoRouter(todoRepository: TodoRepository) extends Router with Directives {
+  import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+  import io.circe.generic.auto._
+
+  override def route: Route = pathPrefix("todos") {
+    pathEndOrSingleSlash {
+      get {
+        complete(todoRepository.all())
+      }
+    }
+  }
+}
