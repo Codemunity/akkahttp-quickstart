ac3fb3954ee79241df66dac34dd40055e8639441
Miguel Lopez <miguell@cakesolutions.net>
2018-08-18 15:00:49 +0100

    Get rid of call by name

--- src/main/scala/TodoDirectives.scala
+++ src/main/scala/TodoDirectives.scala
@@ -8,7 +8,7 @@ trait TodoDirectives extends Directives {
   import io.circe.generic.auto._
 
   def handle[T]
-  (f: => Future[T])
+  (f: Future[T])
   (e: Throwable => ApiError): Directive1[T] = onComplete(f) flatMap {
     case Success(t) =>
       provide(t)
@@ -17,7 +17,7 @@ trait TodoDirectives extends Directives {
       complete(apiError.statusCode, apiError.message)
   }
 
-  def handleWithDefault[T](f: => Future[T]): Directive1[T] =
+  def handleWithDefault[T](f: Future[T]): Directive1[T] =
     handle[T](f)(_ => ApiError.generic)
 
 }
